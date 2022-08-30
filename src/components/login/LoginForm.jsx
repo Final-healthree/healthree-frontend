f86131226b34ce4e73a3664d3b61f99375587f27
YOO JIWAN <yoojiwan@YOOui-MacBookAir.local>
2022-08-30 21:30:22 +0900

    #2 LoginForm 컴퍼넌트 생성, 예시 로그인버튼 이미지, 로고 이미지 삽입

---
+++ src/components/login/LoginForm.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+function LoginForm() {
+
+    return (
+        <div>
+            <img src="https://www.shopintake.com/static/intake/img/sub/kakao_login.jpg" />
+        </div>
+    );
+}
+export default LoginForm
+
