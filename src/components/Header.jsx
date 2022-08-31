91ab49d80d54267c263e2edac3de3637fedee647
YOO JIWAN <yoojiwan@YOOui-MacBookAir.local>
2022-08-31 17:11:26 +0900

    #3 feature 뒤로가기버튼 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 //임시 아이콘
-// import { HiChevronLeft } from "react-icons/hi2";
+import { HiChevronLeft } from "react-icons/hi";
 
 
 const Header = () => {
@@ -10,7 +10,9 @@ const Header = () => {
 
   return (
     <StHeader>
-      {/* <HiChevronLeft onClick={() => navigate(-1)}/> */}
+      <Back>
+      <HiChevronLeft/>
+      </Back>
       <Logo/>
     </StHeader>
   );
@@ -35,9 +37,11 @@ const StHeader = styled.div`
   background-image: url(https://i.pinimg.com/564x/1b/ff/88/1bff88afa70639dedcdd72c2d73ddc84.jpg);
   background-position: center;
   background-size: 175px;
+
+  `
+
+  const Back = styled.div`
+ display: flex;
+ justify-content: flex-start;
   
-  
-  // display: flex;
-  // flex-direction: column;
-  // align-items: center;
   `
