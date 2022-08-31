dfc8e31a2c69261d1b717c7d9bd605a3227e4815
YOO JIWAN <yoojiwan@YOOui-MacBookAir.local>
2022-08-31 12:15:31 +0900

    #3 헤더,풋터 임시 아이콘, 이미지 적용했습니다.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+//임시 아이콘
+// import { HiChevronLeft } from "react-icons/hi2";
+
 
 const Header = () => {
-  return (
-    <StHeaderLayout>
-      <Logo/>
-    </StHeaderLayout>
-  )
-}
+  const navigate = useNavigate();
 
-const StHeaderLayout = styled.div`
-`;
+  return (
+    <StHeader>
+      {/* <HiChevronLeft onClick={() => navigate(-1)}/> */}
+      <Logo/>
+    </StHeader>
+  );
+}
 
 
 export default Header;
@@ -18,27 +22,22 @@ export default Header;
 const StHeader = styled.div`
   width: 100%;
   height: 70px;
-  line-height: 70px;
-
-  padding: 6px 1em 0;
-  box-sizing: border-box;
-  
-  padding: 6px 1em 0;
-  box-sizing: border-box;
 
   display: flex;
   position: relative;
   align-items: center;
+  justify-content: center;
   `
  
   const Logo = styled.div`
-  width: 100%;
+  width: 100px;
   height: 100px;
   background-image: url(https://i.pinimg.com/564x/1b/ff/88/1bff88afa70639dedcdd72c2d73ddc84.jpg);
   background-position: center;
-  background-size: cover;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+  background-size: 175px;
+  
+  
+  // display: flex;
+  // flex-direction: column;
+  // align-items: center;
   `
