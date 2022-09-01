16c02c9e2bbeb11a3d07ad176b9891843af5b8de
yeun38 <choh1997@naver.com>
2022-09-02 03:00:09 +0900

    feat : 달력 범위 고정

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import styled from "styled-components";
-
+import RegCalendar from "../components/register/Calendar";
 const Main = () => {
   return (
     <StMainContainer>
-      Main
+      <RegCalendar />
     </StMainContainer>
-  )
-}     
+  );
+};
 
 const StMainContainer = styled.div`
   width: 100%;
