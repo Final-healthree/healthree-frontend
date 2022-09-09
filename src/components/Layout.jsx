2b5827aff76599b1b27bee425de792676df04891
yeun38 <choh1997@naver.com>
2022-09-09 11:51:44 +0900

    feat : 무한스크롤 범위 오류

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,19 +1,15 @@
 import React from "react";
-import styled from "styled-components"
+import styled from "styled-components";
 
 const Layout = (props) => {
-  return (
-    <StLayout>
-      {props.children}
-    </StLayout>
-  )
-}
+  return <StLayout>{props.children}</StLayout>;
+};
 
 const StLayout = styled.div`
   min-width: 360px;
   max-width: 375px;
-  /* height:100vh; */
-  /* height: calc(100vh - 70px - 70px ); */
+  /* height: 100vh; */
+  /* height: calc(100vh - 70px - 70px); */
   margin: 0 auto;
   background-color: #99c9f2;
 `;
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import styled from "styled-components";
 
 // import MainGoal from "../components/main/MainGoal";
-// import RegCalendar from "../components/register/Calendar";
-import GoalSlider from "../components/main/GoalSlider";
+import RegCalendar from "../components/register/Calendar";
+// import GoalSlider from "../components/main/GoalSlider";
 
 const Main = () => {
   return (
     <StMainContainer>
-      <GoalSlider />
-      {/* <RegCalendar /> */}
+      {/* <GoalSlider /> */}
+      <RegCalendar />
     </StMainContainer>
   );
 };
