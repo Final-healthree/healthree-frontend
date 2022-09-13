1eb844e660c009c2f2e88b56d41bb030bc2b4c77
YOO JIWAN <yoojiwan@YOOui-MacBookAir.local>
2022-09-13 11:46:45 +0900

    #29 Feature 카카오 소셜로그인, server.axios.js 생성, 로컬 스토리지에 토큰 저장

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import MyVideo from "../components/video/MyVideo";
 import styled from "styled-components";
+import serverAxios from "../components/axios/server.axios";
 
 const Mypage = () => {
+  const fetchTodos = async () => {
+    const data = await serverAxios.get(process.env.REACT_APP_REST_API_KEY+"api/users/my_calendar");
+  console.log(data)
+  };
+  
+  useEffect(()=> {fetchTodos()},[])
+  
+
   return (
     <StMainContainer>
       Mypage
