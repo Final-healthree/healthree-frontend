874b5e3650a5c760fab13ecaeaf08902734170de
yeun38 <choh1997@naver.com>
2022-09-14 23:18:04 +0900

    style : 토큰 env 처리

--- src/components/login/LoginForm.jsx
+++ src/components/login/LoginForm.jsx
@@ -2,9 +2,7 @@ import styled from "styled-components";
 import kakaoLogin from "../../assets/kakaoLogin/kakaoLogin.svg";
 
 const LoginForm = () => {
-
-  const token =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7InVzZXJfaWQiOjQsIm5pY2tuYW1lIjoi7Jyg7JiBIiwicHJvZmlsZV9pbWFnZSI6Imh0dHA6Ly9rLmtha2FvY2RuLm5ldC9kbi9ielNMRDgvYnRySTJjd3lZYmcvMDJvTkNvWVVlZXF4czdReVp3a2E2MC9pbWdfNjQweDY0MC5qcGcifSwiaWF0IjoxNjYzMDc4NjM5LCJleHAiOjE2NjMxNjUwMzl9.oMSRn7e4VEuUr8wLZH3VHgoDDFNi0_awEjH6cvZoMDY";
+  const token = process.env.REACT_APP_TOKEN;
   const tokenTest = () => {
     localStorage.setItem("accessToken", token);
   };
