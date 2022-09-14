9e477a7f564a4d014f4b0b1415cfd92db90bb2a9
YOO JIWAN <yoojiwan@YOOui-MacBookAir.local>
2022-09-14 10:16:04 +0900

    view/mypage_calendar 상단 tap 구현 완료

--- src/components/login/LoginForm.jsx
+++ src/components/login/LoginForm.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import kakaoLogin from '../../assets/kakaoLogin/kakaoLogin.svg'
 
 const LoginForm = () => {
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7InVzZXJfaWQiOjQsIm5pY2tuYW1lIjoi7Jyg7JiBIiwicHJvZmlsZV9pbWFnZSI6Imh0dHA6Ly9rLmtha2FvY2RuLm5ldC9kbi9ielNMRDgvYnRySTJjd3lZYmcvMDJvTkNvWVVlZXF4czdReVp3a2E2MC9pbWdfNjQweDY0MC5qcGcifSwiaWF0IjoxNjYyOTkxMDc0fQ.nYSDF0dT_f8EOdmXg-Nhz2lpW194lGsCjouQ1z3fkcc"
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7InVzZXJfaWQiOjQsIm5pY2tuYW1lIjoi7Jyg7JiBIiwicHJvZmlsZV9pbWFnZSI6Imh0dHA6Ly9rLmtha2FvY2RuLm5ldC9kbi9ielNMRDgvYnRySTJjd3lZYmcvMDJvTkNvWVVlZXF4czdReVp3a2E2MC9pbWdfNjQweDY0MC5qcGcifSwiaWF0IjoxNjYzMDU0MDA4LCJleHAiOjE2NjMxNDA0MDh9.3mRTz1aXlCoMWqOnrA6TQW4u84S3EZJhydxSGToL5eY"
     const tokenTest = () => {
           localStorage.setItem("accessToken", token)
     }
