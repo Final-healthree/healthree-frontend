3945faf8401c270d425c4e5bdcb09a15b36a3b6c
YOO JIWAN <yoojiwan@YOOui-MacBookAir.local>
2022-09-15 21:26:35 +0900

    #44 Feature/profile 프로필 컴퍼넌트 생성했습니다.  서버에서 유저 닉네임과 유저 이미지 불러오기 완료

--- src/components/element/Profile.jsx
+++ src/components/element/Profile.jsx
@@ -1,30 +1,54 @@
 import React, { useEffect, useState } from "react";
+import styled from "styled-components";
 import serverAxios from "../axios/server.axios";
 
 function Profile() {
-  const [profileImage, setProfileImage] = useState();
-  const [nickName, setNickName] = useState();
+    const [profileImage, setProfileImage] = useState();
+    const [nickName, setNickName] = useState();
 
-  const getProfile = async () => {
-    await serverAxios
-      .get(process.env.REACT_APP_REST_API_KEY + `api/users/my_calendar`)
-      .then((result) => {
-        console.log(result);
-        setProfileImage(result.data.result.profile);
-        setNickName(result.data.result.nickname);
-      });
-  };
+    const getProfile = async () => {
+        await serverAxios
+            .get(process.env.REACT_APP_REST_API_KEY + `api/users/my_calendar`)
+            .then((result) => {
+                console.log(result);
+                setProfileImage(result.data.result.profile);
+                setNickName(result.data.result.nickname);
+            });
+    };
 
-  useEffect(() => {
-    getProfile();
-  }, []);
+    useEffect(() => {
+        getProfile();
+    }, []);
 
-  return (
-    <div>
-      <img style={{ width: "100px", height: "100px" }} src={profileImage} />
-      <h2> {nickName} </h2>
-    </div>
-  );
+    return (
+        <StArea>
+            <StProfile>
+                <img style={{ width: "34px", height: "34px" }} src={profileImage} />
+            </StProfile>
+            <StNickName> 
+                {nickName} 
+            </StNickName>
+        </StArea>
+    );
 }
 
 export default Profile;
+
+const StArea = styled.div`      
+display: flex;
+align-items: center;
+gap: 10px;
+padding: 0px 0px 10px 0px;
+`
+
+const StProfile = styled.div`
+    display: block;
+    width: 34px; 
+    height: 34px;
+    border-radius: 70%;
+    overflow: hidden;
+
+`
+
+const StNickName = styled.div`
+`
