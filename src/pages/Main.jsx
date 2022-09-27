32a10e2dde304d7ef3c6003bfdb657a863697a2b
yeun38 <choh1997@naver.com>
2022-09-27 20:20:45 +0900

    chore : 오류 코드 수정

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -11,7 +11,6 @@ import { existgoal } from "../redux/modules/existgoal";
 const Main = () => {
   const exist = useSelector((state) => state.existgoal.exist);
   const dispatch = useDispatch();
-  console.log(exist);
 
   if (window.location.href.includes("token")) {
     const [url, token] = window.location.href.split("=");
@@ -22,7 +21,7 @@ const Main = () => {
     await serverAxios
       .get(process.env.REACT_APP_REST_API_KEY + `api/goals/exist`)
       .then((res) => {
-        dispatch(existgoal({ exist: res.data.success }));
+        dispatch(existgoal({ exist: res.data.result }));
       });
   };
 
