e0315bf62ef6747e3821ef6a7437bcb57641264a
YOO JIWAN <yoojiwan@YOOui-MacBookAir.local>
2022-10-04 09:48:29 +0900

    #57 chore/header 로그인 페이지 헤더 숨김

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
-//svg 이미지
 import headerLogo from "../../assets/headerFooter/headerLogo.svg";
-// import back from "../assets/images/back.svg";
 
 const Header = () => {
   const navigate = useNavigate();
   //로그인 페이지 헤더 숨기기
+  if (window.location.pathname === "/") return null;
+
   const token = localStorage.getItem("Token");
   if (token === null) return null;
   
