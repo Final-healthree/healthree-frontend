7c32a6762cf00e29d1b84806cd9c27052258836a
YOO JIWAN <yoojiwan@YOOui-MacBookAir.local>
2022-11-04 17:11:29 +0900

    chore/login

--- src/components/kakaoLogin/LoginForm.jsx
+++ src/components/kakaoLogin/LoginForm.jsx
@@ -5,7 +5,7 @@ const LoginForm = () => {
   return (
     <div>
       <StLoginArea>
-        <a href={process.env.REACT_APP_REST_API_KEY + "api/auth/kakao"}>
+        <a href={"api/auth/kakao"}>
           <img src={kakaoLogin} />
         </a>
       </StLoginArea>
